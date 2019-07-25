811d59ae635535577557fd2d527f9e0bf3892d0d
Geza Csiki <geza.csiki@cashpresso.com>
2019-07-25 14:51:26 +0200

    Available for carthage

---
+++ Carthage/Build/iOS/SCTqrScanner.framework/Headers/SCTqrScanner.h
@@ -0,0 +1,19 @@
+//
+//  SCTqrScanner.h
+//  SCTqrScanner
+//
+//  Created by Geza Csiki on 30.11.18.
+//  Copyright © 2018 Geza Csiki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SCTqrScanner.
+FOUNDATION_EXPORT double SCTqrScannerVersionNumber;
+
+//! Project version string for SCTqrScanner.
+FOUNDATION_EXPORT const unsigned char SCTqrScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SCTqrScanner/PublicHeader.h>
+
+
